d3ff54ce6186765fa3d8d9fe8bbd800c516a5494
Nidhin Joseph <nidhinjoseph.1990@gmail.com>
2023-03-13 18:12:17 -0300

    Flyweight pattern

---
+++ L06/Flyweight/src/Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+
+  public static void main(String[] args) {
+    show();
+  }
+  public static void show() {
+    var sheet = new SpreadSheet();
+    sheet.setContent(0, 0, "Hello");
+    sheet.setContent(1, 0, "World");
+    sheet.setFontFamily(0, 0, "Arial");
+    sheet.render();
+  }
+}
---
+++ L06/Flyweight/src/com/designpattern/flyweight/Demo.java
@@ -0,0 +1,16 @@
+package com.designpattern.flyweight;
+
+public class Demo {
+
+  public static void main(String[] args) {
+    show();
+  }
+  public static void show() {
+    var contextFactory = new CellContextFactory();
+    var sheet = new SpreadSheet(contextFactory);
+    sheet.setContent(0, 0, "Hello");
+    sheet.setContent(1, 0, "World");
+    sheet.setFontFamily(0, 0, "Arial");
+    sheet.render();
+  }
+}
